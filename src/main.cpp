75b75758a8e3ce95a31595e9a21c49445956792d
Fran Varas <tatico@gmail.com>
2017-08-27 16:26:33 +0200

    added a function to get DGT data

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 
 #define DHTPIN  0       // what digital pin the DHT22 is conected to
 #define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
-
 DHT dht(DHTPIN, DHTTYPE);
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 // sensor/hum
@@ -16,32 +16,7 @@ PubSubClient client(espClient);
 char tmp[11];
 char hum[11];
 
-void setup()
-{
-    Serial.begin(9600);
-
-    // Set WiFi to station mode and disconnect from an AP if it was previously connected
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(100);
-    Serial.print(ssid);
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-          delay(500);
-          Serial.print(".");
-      }   
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    Serial.println("Setup done");
-    client.setServer("bike.dragon-tortuga.net", 1883);
-    client.connect("widora1");
-}
-
-void loop() {
+void DHT_get_data() {
     float h = dht.readHumidity();
     delay(300);
     float t = dht.readTemperature();
@@ -60,10 +35,41 @@ void loop() {
     Serial.println(WiFi.localIP());
     String tmp = String(t);
     String hum = String(h);
-    client.publish("sensor/hum", (char*) hum.c_str());
-    client.publish("sensor/tmp", (char*) tmp.c_str());
-    //client.disconnect();
-    esp_deep_sleep(1500000000);
-    //esp_deep_sleep(15000000);
-    
+    if (!client.connected()){
+      client.connect("widora1");
+      delay(1000);
+      return;
+    } else {
+      client.publish("sensor/hum", (char*) hum.c_str());
+      client.publish("sensor/tmp", (char*) tmp.c_str());
+      client.disconnect();
+      esp_deep_sleep(1500000000);
+      //esp_deep_sleep(15000000);
+    }
+}
+
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.print(ssid);
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          Serial.print(".");
+      }   
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("Setup done");
+    client.setServer("bike.dragon-tortuga.net", 1883);
+    client.connect("widora1");
+    client.disconnect();
+}
+
+void loop() {
+  DHT_get_data(); 
 }
